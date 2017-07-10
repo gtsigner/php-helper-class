ed95d6332a87bd999c241b457153fde8f4f70789
unknown <zhaojunlike@gmail.com>
2017-07-10 15:28:19 +0800

    full

---
+++ helper/src/DayuHelper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QQ: 1790404212
+ * Author：软件开发,网站建设,网站优化/
+ * Date: 2016/8/16
+ * Time: 8:31
+ */
+
+namespace Oeynet\Helper;
+
+/**
+ * 阿里大雨
+ * Class DayuHelper
+ * @package Common\Helper
+ */
+class DayuHelper
+{
+
+
+}
---
+++ helper/src/SmsHelper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QQ: 1790404212
+ * Author：软件开发,网站建设,网站优化/
+ * Date: 2016/8/16
+ * Time: 8:32
+ */
+
+namespace Oeynet\Helper;
+
+
+class SmsHelper
+{
+
+
+    public static function sendSms($phone, $content)
+    {
+
+    }
+}
