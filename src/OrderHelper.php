a2169d1d70165f875c432db57b937fe15d371cd9
zhaojunlike <zhaojunlike@gmail.com>
2017-08-05 11:56:26 +0800

    libs

---
+++ src/OrderHelper.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QQ: 209900956
+ * oschina:http://git.oschina.net/zhaojunlike
+ * github: http://github.com/zhaojunlike
+ * Author：软件开发，网站建设/
+ * Date: 2016/8/7
+ * Time: 8:25
+ */
+
+namespace Oeynet\Helper;
+
+
+class OrderHelper
+{
+
+    public static function createOrderNo()
+    {
+        $year_code = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+        return date('Ymd') . $year_code[intval(date('Y')) - 2010] .
+        strtoupper(dechex(date('m'))) . date('d') .
+        substr(time(), -5) . substr(microtime(), 2, 5) . sprintf('d', rand(0, 99));
+    }
+}
